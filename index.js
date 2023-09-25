05d6452ec9429f4112dabc222e6102ef7d90cd62
James B <jamesbhattarai14@gmail.com>
2023-09-25 20:18:33 +0545

    websocket works fine

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
-const express = require("express");
+const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
+const WebSocket = require('ws');
+const Order = require('./models/order'); // Update the path as needed
+
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-const mongoose = require("mongoose");
-const path = require("path");
-//  dbConnection()
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+require('dotenv').config();
+// Connect to your MongoDB database
+mongoose.connect(process.env.URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const dbConnection = require("./lib/dbConnection");
+mongoose.connection.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
 
-//routes
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
-const salesRoute = require("./routes/salesRoute");
-app.use("/", salesRoute);
+// Define your WebSocket logic
+wss.on('connection', (ws) => {
+  console.log('WebSocket client connected');
+  ws.send("hello from the server")
+  // Send JSON data from the database when a WebSocket connection is established
+  Order.find({})
+    .exec()
+    .then((salesData) => {
+      ws.send(JSON.stringify(salesData));
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
 
-const PORT = 4000;
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
+  // Handle WebSocket messages (if needed)
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+  });
+
+  // Handle WebSocket closure
+  ws.on('close', () => {
+    console.log('WebSocket client disconnected');
+  });
+});
+
+// Define your Express routes (salesController.js)
+const salesController = require('./controller/sales'); // Update the path as needed
+
+app.get('/sales', salesController.getAllSales);
+
+// Start the Express server
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
